44bb19917b051f8538b6a21b665e77adc87e26a1
Edison200316 <edison.zamora9535@utc.edu.ec>
2024-10-29 10:01:44 -0500

    Create funcion.py

---
+++ funcion.py
@@ -0,0 +1,18 @@
+def sumar(n1,n2):
+    return n1 + n2
+
+def restar(n1,n2):
+    return n1 - n2
+
+def multiplicar(n1,n2):
+    return n1 * n2
+
+def dividir(n1,n2):
+    return n1 / n2
+n1 = int (input("Ingresa el numero 1: "))
+n2 = int (input("Ingresa el numero 2: "))
+
+print("Suma: ", sumar(n1, n2))
+print("Resta: ", restar(n1, n2))
+print("Multicar: ", multiplicar(n1, n2))
+print("Dividir: ", dividir(n1, n2))
